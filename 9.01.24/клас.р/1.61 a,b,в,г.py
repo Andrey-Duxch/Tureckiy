428ee8e58d208d14c11e153b099b5e085f0345c8
student <o@gg>
2024-01-09 13:24:00 +1200

    df

---
+++ 9.01.24/клас.р/1.61 a,b,в,г.py
@@ -0,0 +1,17 @@
+a = int(input())
+a2 = a*a
+a4 = a2*a2
+print(a4)
+a2 = a*a
+a4 = a2*a2
+a6 = a4*a2
+print(a6)
+a2 = a*a
+a4 = a2*a2
+a6 = a4*a2
+a7 = a6*a
+print(a7)
+a2 = a*a
+a4 = a2*a2
+a8 = a4*a4
+print(a8)
