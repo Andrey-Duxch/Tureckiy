3eda4ff3d424ef1efdac5263b926b5184aaf1e63
student <o@gg>
2024-01-12 10:17:17 +1200

    df

---
+++ 12.01.23/кл.р/1.py
@@ -0,0 +1,7 @@
+from random import *
+a = randint(1, 6)
+b = randint(1,6)
+c = randint(1,6)
+print(f'Выпало очков:\n{a} {b} {c}')
+d = (a+b+c)/3
+print(f' ({a} + {b} +{c})/3 =', d)
---
+++ 12.01.23/кл.р/6.py
@@ -0,0 +1,16 @@
+a = int(input("Возраст Антона"))
+b = int(input("Возраст Бориса"))
+c = int(input("Возраст Виктора"))
+if a > b and a > c:
+    print("Антон старше всех")
+elif b > a and b > c:
+    print("Борис старше всех")
+elif c > a and c > b:
+    print("Виктор старше всех")
+elif a == b > c:
+    print("Антон и Борис старше Виктора")
+elif b == c > a:
+    print("Борис и Виктор старше Антона")
+elif a == c > b:
+    print("Антон и Виктор старше Бориса")
+
