8a461721276ac9247a30527ddab14d91ee6b2407
Andrey-Duxch <andreytureckiy1706@gmail.com>
2024-02-12 18:57:35 +1200

    34

---
+++ 09.02.24/2.py
@@ -0,0 +1,15 @@
+import math
+def a(n):
+    if n == 2 or n == 3:
+        return "Простое"
+    if n <= 1 or n % 2 == 0:
+        return "Составное"
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return "Составное"
+    return "Простое"
+
+
+for i in range(1,10000):
+    print(f"{i} - {a(i)}")
+
