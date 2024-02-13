c04e97a378609f7fb2e192ee5d99afef4a8a883a
student <o@gg>
2024-02-13 12:11:06 +1200

    df

--- 09.02.24/1.py
+++ 09.02.24/1.py
@@ -1,8 +1,9 @@
-def f(n):
+def a(n):
     k = 0
     while n > 0:
         b = n % 10
         n = n // 10
         k += b
     return k
-print(f(123))
+n = int(input("Напишите число:"))
+print(f"Сумма цифр {n} = {a(n)}")
