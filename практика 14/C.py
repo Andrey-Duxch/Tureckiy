f7e4304fece5868b0774d09816f6e735e9c38c9f
Andrey-Duxch <andreytureckiy1706@gmail.com>
2024-04-08 18:42:42 +1200

    34

--- практика 14/C.py
+++ практика 14/C.py
@@ -3,10 +3,9 @@ n = int(input("Введите количество элементов:\n"))
 A = [randint(0, 5) for _ in range(n)]
 print(f'Массив:\n{" ".join(str(i) for i in A)}')
 s = []
-for i in range(len(A)):
-    for j in range(i + 1, len(A)):
-        if A[i] == A[j] and A[i] not in s:
-            s.append(A[i])
+for i in A:
+    if A.count(i) > 1 and i not in s:
+        s.append(i)
 if s:
     print("Есть:", ", ".join(str(i) for i in s))
 else:
